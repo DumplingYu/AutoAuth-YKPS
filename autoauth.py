9c0d0ffc811b40cf5277bdd6d45bfad9d1d652f1
DumplingYu <dumpling_yu@163.com>
2016-08-12 21:53:25 +0700

    Create autoauth.py

---
+++ autoauth.py
@@ -0,0 +1,31 @@
+import socket, uuid, urllib, requests # Import Modules
+
+requests.packages.urllib3.disable_warnings() #Disable Warnings
+
+#Get Private IP Address and MAC Address
+ipAddr = socket.gethostbyname(socket.gethostname())
+macAddr = ':'.join([uuid.UUID(int=uuid.getnode()).hex[-12:].upper()[i:i+2] for i in range(0,11,2)])
+
+#Get Username and Password
+with open('user.dat') as f:
+    username = f.readline().strip()
+    password = f.readline().strip()
+
+#Web Authentication
+url = 'https://auth.ykpaoschool.cn/portalAuthAction.do'
+form_data = {'wlanuserip':ipAddr,'mac':macAddr,'wlanacname':'hh1u6p','wlanacIp':'192.168.186.2','userid':username,'passwd':password}
+
+try:
+    requests.post(url,data=form_data,verify=False)
+except Exception as e:
+    print('An Error Occurred: %s'%e)
+
+#The Blue Auth Page
+url = 'http://192.168.1.1:8181/'
+authServ = 'KVW4EtNMbo5/+u85r456zjg2Mcg+SdmChszWCQWjWiXl1Hb1bV/hhqkkc1jK1CqKoWU4hu8EafvmvGBr9L74aRoQjMkO3nxJWNpRwQ51eSQlnpPK7WOMS0iWLxaoWod9na30aL17FfRXPHx6t5EkTCdCuL9YtXBacRji4GkjVThFKdRvRKIK0tzZFSmzxZbwGdu1SIyuWQrO7+LKyTsY+sHM6oxNyo3QUJsnjzH/GiMw6N6eJE1JgJ4PUam7+zoYqJPZMeFMWbRU8Spwt0KKmgsR7I8+hLT/xyr7FVOXQOMtwYbG83TNVnp95XDy9WPo3Rj0mDZtBOuQWjRyVRzl96gU0J76iQNbwjcB16mkQIrIsrmhNnxj94iZqv4rprdqkX87YMReXlt00YsomeEvU7kFfuuncY1iqYn/QMi3folyrlB5iyGlhjql5/zyRso3VPkE4Vn7qhsAl0c='
+form_data = {'txtUserName':username,'txtPasswd':password,'authServ':authServ}
+
+try:
+    requests.post(url,data=form_data,verify=False)
+except Exception as e:
+    print('An Error Occurred: %s'%e)
