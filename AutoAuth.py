e2986f8bd98da85cbbde4b97167ae48c11275536
George Yu <dumpling_yu@163.com>
2017-03-29 12:33:50 +0800

    Update AutoAuth.py

--- AutoAuth.py
+++ AutoAuth.py
@@ -1,32 +1,36 @@
 # AutoAuth - YKPS
-# 3.1.3
+# 3.1.4
 # Author: George Yu
 # URL: https://github.com/yu-george/AutoAuth-YKPS/
 
-VERSION = '3.1.3'
+VERSION = '3.1.4'
 
 try:
-    import os
-    import requests
+    from requests import get, post
+    from requests.exceptions import Timeout
+    from os import system, popen, path
+    from sys import version_info
     from socket import gethostbyname, gethostname, getfqdn
     from uuid import UUID, getnode
     from urllib.parse import unquote
     from base64 import b64decode
     from threading import Thread
+    from re import compile
 except ImportError:
     notif('Error while importing [0x01]')
 
-def notif(message):
-    os.system('osascript -e \'display notification "{}" with title "AutoAuth" sound name "Basso"\''.format(message))
-    exit()
+def notif(message, exitAfter=True, soundName='Basso'):
+    system('osascript -e \'display notification "{}" with title "AutoAuth" sound name "{}"\''.format(message, soundName))
+    if exitAfter:
+        exit()
 
 def check_update():
     try:
-        web = requests.get('https://raw.githubusercontent.com/yu-george/AutoAuth-YKPS/master/version', timeout=1.5)
+        web = get('https://raw.githubusercontent.com/yu-george/AutoAuth-YKPS/master/version', timeout=1.5)
         latest = web.text.strip()
         if latest != VERSION:
-            os.system('osascript -e \'display notification "Update Available!" with title "AutoAuth" sound name "Glass"\'')
-    except requests.exceptions.Timeout:
+            notif('Update Available!', False)
+    except Timeout:
         pass
     except Exception:
         pass
@@ -40,7 +44,7 @@ def login_webauth(): # Web Authentication
                      'wlanacIp': '192.168.186.2',
                      'userid': username,
                      'passwd':password}
-        requests.post(url, data=form_data, verify=False)
+        post(url, data=form_data, verify=False)
     except Exception:
         notif('Error while logging in the web authentication [0x04]')
 
@@ -49,9 +53,8 @@ def login_blueauth(): # The Blue Auth Page
         headers = {'User-Agent': 'Mozilla/5.0',
                    'Accept-Encoding': 'gzip, deflate, sdch',
                    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6'}
-        web = requests.get('http://www.apple.com/cn/', allow_redirects=True, headers=headers)
-        oldURL = unquote(web.url.split('oldURL=')[1].split('&')[0])
-        authServ = unquote(web.url.split('authServ=')[1])
+        web = get('http://www.apple.com/cn/', allow_redirects=True, headers=headers)
+        oldURL, authServ = compile(r'oldURL=([^&]+)&authServ=(.+)').findall(unquote(web.url))[0]
     except IndexError:
         return
     except Exception:
@@ -62,7 +65,7 @@ def login_blueauth(): # The Blue Auth Page
                      'txtPasswd': password,
                      'oldURL': oldURL,
                      'authServ': authServ}
-        requests.post(url, data=form_data, verify=False)
+        post(url, data=form_data, verify=False)
     except Exception:
         notif('Error while loggin in the blue auth page [0x06]')
 
@@ -70,18 +73,31 @@ def auth():
     login_webauth()
     login_blueauth()
 
+print(version_info)
+if version_info[0] < 3:
+    notif('Python version too old [0x00]')
+
 try:
-    requests.packages.urllib3.disable_warnings() # Disable Warnings
     # Get Private IP Address and MAC Address
-    ipAddr = gethostbyname(getfqdn())
+    try:
+        ipAddr = gethostbyname(gethostname())
+    except Exception:
+        try:
+            ipAddr = gethostbyname(getfqdn())
+        except Exception:
+            ipAddr = popen("ifconfig | grep 'inet ' | grep -v '127.0' | xargs | awk -F '[ :]' '{print $2}'").readline().strip()
+            if not ipAddr:
+                raise Exception() # This exception will be excepted by the very outer except
     macAddr = ':'.join([UUID(int=getnode()).hex[-12:].upper()[i:i+2] for i in range(0,11,2)])
 except Exception:
     notif('Error while initializing [0x02]')
 
 try: # Get Username and Password
-    file = open(os.path.expanduser('~/Library/Application Support/AutoAuth/usr.dat'))
+    file = open(path.expanduser('~/Library/Application Support/AutoAuth/usr.dat'))
     username = file.readline().strip()
     password = b64decode(file.readline().strip().encode()).decode()
+    if not username or not password:
+        raise Exception() # This exception will be excepted by the very outer except
 except Exception:
     notif('Error while getting user info [0x03]')
 
